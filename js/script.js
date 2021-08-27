77a189b3fe2e35a072eca6578d429a62c1b728d3
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-08-27 01:24:04 -0400

    use sound level to transform the width of the shape

--- js/script.js
+++ js/script.js
@@ -1,50 +1,35 @@
 console.clear()
 
-let posX = 0
-let posY = 0
+let audio
+let amp
 
 
 function preload() {
-
+  audio = loadSound('audio/3.mp3')
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
   rectMode(CENTER);
-  strokeWeight(20)
+
+  audio.play()
+  amp = new p5.Amplitude()
+
 }
 
 function draw() {
   background(0)
   fill(255, 204, 0)
-  stroke('white')
 
   translate(width / 2, height / 2)
 
-  const mapX = map(mouseX, 0, width, 0, 500)
-  const mapY = map(mouseY, 0, height, 0, 500)
-  rect(posX, posY, mapX, mapY)
+  const volume = amp.getLevel()
+  const mapW = map(volume, 0, 1, 0, 500)
+
+  rect(0, 0, mapW, 500)
 }
 
 function windowResized() {
 
 }
 
-function keyPressed() {
-  switch (keyCode) {
-    case LEFT_ARROW:
-      posX -= 10
-      break
-    case RIGHT_ARROW:
-      posX += 10
-      break
-    case UP_ARROW:
-      posY -= 10
-      break
-    case DOWN_ARROW:
-      posY += 10
-      break
-    default:
-      break
-  }
-}
