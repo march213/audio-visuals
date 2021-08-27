536aa90265557ac3e3b7a9b1d5d4202c38d93e6c
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-08-26 22:45:25 -0400

    some updates

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@ function preload() {
 }
 
 function setup() {
-
+  createCanvas(windowWidth, windowHeight)
+  rectMode(CENTER);
 }
 
 function draw() {
-
+  translate(width / 2, height / 2)
+  fill(255, 204, 0)
+  rect(0, 0, 500, 500)
 }
 
 function windowResized() {
