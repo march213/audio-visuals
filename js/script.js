fadd170a1c1d76e7d93f07e140073f271cc8a66a
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-08-27 00:45:43 -0400

    use mouse position

--- js/script.js
+++ js/script.js
@@ -8,12 +8,18 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight)
   rectMode(CENTER);
+  strokeWeight(20)
 }
 
 function draw() {
-  translate(width / 2, height / 2)
+  background(0)
   fill(255, 204, 0)
-  rect(0, 0, 500, 500)
+  stroke('white')
+  translate(width / 2, height / 2)
+
+  const mapX = map(mouseX, 0, width, 0, 500)
+  const mapY = map(mouseY, 0, height, 0, 500)
+  rect(0, 0, mapX, mapY)
 }
 
 function windowResized() {
