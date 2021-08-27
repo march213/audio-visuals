e213a532e39280a9e05b75f3d2585d3620331ef7
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-08-27 01:38:34 -0400

    use fast fourier transform to draw waves

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ console.clear()
 
 let audio
 let amp
+let fft
 
 
 function preload() {
-  audio = loadSound('audio/3.mp3')
+  audio = loadSound('audio/2.mp3')
 }
 
 function setup() {
@@ -13,20 +14,32 @@ function setup() {
   rectMode(CENTER);
 
   audio.play()
-  amp = new p5.Amplitude()
-
+  // amp = new p5.Amplitude()
+  fft = new p5.FFT()
 }
 
 function draw() {
   background(0)
+  stroke(255, 204, 0)
   fill(255, 204, 0)
 
-  translate(width / 2, height / 2)
 
-  const volume = amp.getLevel()
-  const mapW = map(volume, 0, 1, 0, 500)
+  // const volume = amp.getLevel()
+  // const mapW = map(volume, 0, 1, 0, 500)
+  // rect(0, 0, mapW, 500)
 
-  rect(0, 0, mapW, 500)
+  // const waveform = audio.getPeaks()
+  // for (let i = 0; i < waveform.length; i++) {
+  //   line(i, waveform[i] * 10, i, waveform[i] * -10)
+  // }
+
+  const waveform = fft.waveform()
+
+  for (let i = 0; i < waveform.length; i++) {
+    const x = map(i, 0, waveform.length, 0, width)
+    const y = map(waveform[i], -1, 1, 0, height)
+    point(x, y)
+  }
 }
 
 function windowResized() {
