630d03ad8843d1bbcb0855961d8342840c3bd9f3
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-08-27 22:01:21 -0400

    use fft analyze for visual

--- js/script.js
+++ js/script.js
@@ -1,44 +1,39 @@
 console.clear()
 
 let audio
-let amp
 let fft
+let bins = 1024
+let binWidth
 
 
 function preload() {
-  audio = loadSound('audio/2.mp3')
+  audio = loadSound('audio/4.mp3')
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  rectMode(CENTER);
-
-  audio.play()
-  // amp = new p5.Amplitude()
-  fft = new p5.FFT()
+  const canvas = createCanvas(windowWidth, windowHeight)
+  canvas.mouseClicked(togglePlay)
+  fft = new p5.FFT(0, bins)
+  binWidth = width / bins
 }
 
 function draw() {
   background(0)
-  stroke(255, 204, 0)
-  fill(255, 204, 0)
+  noStroke();
 
+  const spectrum = fft.analyze()
 
-  // const volume = amp.getLevel()
-  // const mapW = map(volume, 0, 1, 0, 500)
-  // rect(0, 0, mapW, 500)
+  for (let i = 0; i < spectrum.length; i++) {
+    const y = map(spectrum[i], 0, 255, height, 0)
+    rect(i * binWidth, y, binWidth, height - y)
+  }
+}
 
-  // const waveform = audio.getPeaks()
-  // for (let i = 0; i < waveform.length; i++) {
-  //   line(i, waveform[i] * 10, i, waveform[i] * -10)
-  // }
-
-  const waveform = fft.waveform()
-
-  for (let i = 0; i < waveform.length; i++) {
-    const x = map(i, 0, waveform.length, 0, width)
-    const y = map(waveform[i], -1, 1, 0, height)
-    point(x, y)
+function togglePlay() {
+  if (audio.isPlaying()) {
+    audio.pause()
+  } else {
+    audio.loop()
   }
 }
 
