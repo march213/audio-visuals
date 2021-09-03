77438eb04f9bcb52d7c1addff296f3bcffb71b86
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-09-02 22:20:33 -0400

    settip up shaders

---
+++ js/audio-analysis.js
@@ -0,0 +1,45 @@
+console.clear()
+
+let audio
+let fft
+let peakDetect
+const bins = 64
+let bgColor = 0
+
+function preload() {
+  audio = loadSound('audio/4.mp3')
+}
+
+function setup() {
+  const canvas = createCanvas(windowWidth, windowHeight)
+  canvas.mouseClicked(togglePlay)
+  fft = new p5.FFT()
+  peakDetect = new p5.PeakDetect(400, 2600, 0.1);
+  peakDetect.onPeak(peakDetected)
+
+}
+
+function draw() {
+  background(bgColor)
+  noStroke();
+
+  fft.analyze(bins)
+  peakDetect.update(fft);
+}
+
+function peakDetected() {
+  bgColor = color(random(255), random(255), random(255))
+}
+
+function togglePlay() {
+  if (audio.isPlaying()) {
+    audio.pause()
+  } else {
+    audio.loop()
+  }
+}
+
+function windowResized() {
+
+}
+
--- js/script.js
+++ js/script.js
@@ -1,45 +1,12 @@
-console.clear()
-
-let audio
-let fft
-let peakDetect
-const bins = 64
-let bgColor = 0
+let shaders
 
 function preload() {
-  audio = loadSound('audio/4.mp3')
+  shaders = loadShader('../shaders/vertext.vert', '../shaders/fragment.frag')
 }
-
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight)
-  canvas.mouseClicked(togglePlay)
-  fft = new p5.FFT()
-  peakDetect = new p5.PeakDetect(400, 2600, 0.1);
-  peakDetect.onPeak(peakDetected)
-
+  createCanvas(windowWidth, windowHeight, WEBGL)
+  shader(shaders)
 }
-
 function draw() {
-  background(bgColor)
-  noStroke();
-
-  fft.analyze(bins)
-  peakDetect.update(fft);
+  rect(0, 0, width, height)
 }
-
-function peakDetected() {
-  bgColor = color(random(255), random(255), random(255))
-}
-
-function togglePlay() {
-  if (audio.isPlaying()) {
-    audio.pause()
-  } else {
-    audio.loop()
-  }
-}
-
-function windowResized() {
-
-}
-
